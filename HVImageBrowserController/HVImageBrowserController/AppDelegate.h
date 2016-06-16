6b9edd2bd692cd7a2fc5f8ecd9915dbb08b7120b
liujianli <liujianli@huivo.com>
2016-06-16 14:07:02 +0800

    HVImageBrowserController 基础

---
+++ HVImageBrowserController/HVImageBrowserController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HVImageBrowserController
+//
+//  Created by BJ13041603-002 on 16/6/16.
+//  Copyright © 2016年 liujianli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HVImageBrowserController/HVImageBrowserController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HVImageBrowserController
+//
+//  Created by BJ13041603-002 on 16/6/16.
+//  Copyright © 2016年 liujianli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
